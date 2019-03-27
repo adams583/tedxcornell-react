3cf4c8dc247d6185e87dfd9db61d8a531841c89c
Adam Skrocki <ams792@cornell.edu>
2019-03-26 22:28:57 -0400

    Updates

---
+++ precache-manifest.7e6bf1190185d56f89bc9cd8aa2161a9.js
@@ -0,0 +1,282 @@
+self.__precacheManifest = [
+  {
+    "revision": "f06729986c341b721e443667224dcefa",
+    "url": "/tedxcornell/static/media/lou.f0672998.jpg"
+  },
+  {
+    "revision": "27ca6e8759804e3c59d3",
+    "url": "/tedxcornell/static/css/main.2cca757a.chunk.css"
+  },
+  {
+    "revision": "106a0d38f3336ca6a4fc8512179ab52e",
+    "url": "/tedxcornell/static/media/partnership.106a0d38.jpg"
+  },
+  {
+    "revision": "ca1410bf79aa3ef6519f",
+    "url": "/tedxcornell/static/js/1.ca1410bf.chunk.js"
+  },
+  {
+    "revision": "d460a27f6b7bc7198ec4",
+    "url": "/tedxcornell/static/js/runtime~main.d460a27f.js"
+  },
+  {
+    "revision": "09f20861d5758b2fd098e6de13d25c53",
+    "url": "/tedxcornell/static/media/aida-mollenkamp-background.09f20861.jpg"
+  },
+  {
+    "revision": "40be36bd162c1cc3783858ff901c54b4",
+    "url": "/tedxcornell/static/media/jessenitai2.40be36bd.jpg"
+  },
+  {
+    "revision": "6dd88cb6d4715130f58ecec740fcccf2",
+    "url": "/tedxcornell/static/media/logo1.6dd88cb6.png"
+  },
+  {
+    "revision": "ff003e1920b832b65e5f08332176ef0e",
+    "url": "/tedxcornell/static/media/itai1.ff003e19.jpg"
+  },
+  {
+    "revision": "394504eec23fa12b491409ec53caf107",
+    "url": "/tedxcornell/static/media/yamatai.394504ee.jpg"
+  },
+  {
+    "revision": "0ed45af5965c54fcc4cfd4e6bb990e66",
+    "url": "/tedxcornell/static/media/jessencolin.0ed45af5.jpg"
+  },
+  {
+    "revision": "5fd0b66f87d3a0a4df9304023ab1670c",
+    "url": "/tedxcornell/static/media/events_bg1.5fd0b66f.jpg"
+  },
+  {
+    "revision": "0faf0dace59e114faafa4639c759f924",
+    "url": "/tedxcornell/static/media/maryanna.0faf0dac.jpg"
+  },
+  {
+    "revision": "544fe364115e7dbef58094722cc303d2",
+    "url": "/tedxcornell/static/media/howard.544fe364.jpg"
+  },
+  {
+    "revision": "613c5bc3280b7b988646a2cafb81e178",
+    "url": "/tedxcornell/static/media/aida-mollenkamp.613c5bc3.jpg"
+  },
+  {
+    "revision": "c4c3660de6e2be9359e57a3eca791111",
+    "url": "/tedxcornell/static/media/michael.c4c3660d.jpg"
+  },
+  {
+    "revision": "68cc41ec33970993486cea886038d877",
+    "url": "/tedxcornell/static/media/mason-peck.68cc41ec.jpg"
+  },
+  {
+    "revision": "d41bf312ccc88c9636d97db3ebc8ce53",
+    "url": "/tedxcornell/static/media/david_pic.d41bf312.jpeg"
+  },
+  {
+    "revision": "a6f9069fef623986a679476f44371735",
+    "url": "/tedxcornell/static/media/ifeoma_pic.a6f9069f.jpg"
+  },
+  {
+    "revision": "2a19147d97a28fe9f782dd59ca28dd71",
+    "url": "/tedxcornell/static/media/susan_pic.2a19147d.jpeg"
+  },
+  {
+    "revision": "5ebfe720340468ca9b3715130099ba72",
+    "url": "/tedxcornell/static/media/drew_pic.5ebfe720.jpeg"
+  },
+  {
+    "revision": "c1329ce62a219eebfc3bce1206da6c82",
+    "url": "/tedxcornell/static/media/itai_pic.c1329ce6.jpeg"
+  },
+  {
+    "revision": "473be344e4e1f7c174a23e2212da4918",
+    "url": "/tedxcornell/static/media/colin_pic.473be344.jpg"
+  },
+  {
+    "revision": "2914defd1e9d888be7c941112fd49698",
+    "url": "/tedxcornell/static/media/renee_pic.2914defd.jpg"
+  },
+  {
+    "revision": "9da5ad4640f1fa21b7ea38d87b79af6b",
+    "url": "/tedxcornell/static/media/shimon_pic.9da5ad46.jpeg"
+  },
+  {
+    "revision": "abb2f798781e0636857021920194e8a5",
+    "url": "/tedxcornell/static/media/fredrik_pic.abb2f798.jpeg"
+  },
+  {
+    "revision": "ee62b2b2598763a1fbd8475a23849a2d",
+    "url": "/tedxcornell/static/media/jon_pic.ee62b2b2.jpeg"
+  },
+  {
+    "revision": "1ef8954038ba663383aaea5c01615640",
+    "url": "/tedxcornell/static/media/barbara_pic.1ef89540.jpeg"
+  },
+  {
+    "revision": "8c934d9773eea331052e6c59b7eb74a5",
+    "url": "/tedxcornell/static/media/karim_pic.8c934d97.jpg"
+  },
+  {
+    "revision": "9334adcb04bec130302c4dc745fc46dd",
+    "url": "/tedxcornell/static/media/robert_pic.9334adcb.jpeg"
+  },
+  {
+    "revision": "05545f791b071aa94fe2ec1cdab00d86",
+    "url": "/tedxcornell/static/media/adarsh_pic.05545f79.jpeg"
+  },
+  {
+    "revision": "b84f19aa288e850e716e79c266109385",
+    "url": "/tedxcornell/static/media/nick_pic.b84f19aa.jpeg"
+  },
+  {
+    "revision": "f75ec1e0cb699ae9dc6ac55d8ed79aa3",
+    "url": "/tedxcornell/static/media/miwa_pic.f75ec1e0.jpeg"
+  },
+  {
+    "revision": "e61d74e60d60f3144dbb9a97a2e73ceb",
+    "url": "/tedxcornell/static/media/scott_pic.e61d74e6.jpeg"
+  },
+  {
+    "revision": "a1d2d07c53e70cf6024cf7f805a35320",
+    "url": "/tedxcornell/static/media/andrew.a1d2d07c.jpg"
+  },
+  {
+    "revision": "27ca6e8759804e3c59d3",
+    "url": "/tedxcornell/static/js/main.27ca6e87.chunk.js"
+  },
+  {
+    "revision": "f3164ea89cf048615ce44ab789ab5379",
+    "url": "/tedxcornell/static/media/chandler.f3164ea8.jpg"
+  },
+  {
+    "revision": "160a491be1cbe79ee9e169abbe42ecd0",
+    "url": "/tedxcornell/static/media/howard-zucker-background.160a491b.jpg"
+  },
+  {
+    "revision": "b994305355a0d7dbb8f1ae474ffa6385",
+    "url": "/tedxcornell/static/media/mason-peck-background.b9943053.jpg"
+  },
+  {
+    "revision": "9630074f6e3342c1b22db3b465c6825d",
+    "url": "/tedxcornell/static/media/michael-graziano-background.9630074f.jpg"
+  },
+  {
+    "revision": "527023c52f5547139401193f5e501090",
+    "url": "/tedxcornell/static/media/maryanna-saenko-background.527023c5.jpg"
+  },
+  {
+    "revision": "0d3d4b5069b04c1bdd973706b1c66d0d",
+    "url": "/tedxcornell/static/media/tickets_stock.0d3d4b50.jpg"
+  },
+  {
+    "revision": "81ea44c663d9866eb33025e829fa5dc4",
+    "url": "/tedxcornell/static/media/itai3.81ea44c6.jpg"
+  },
+  {
+    "revision": "e164b0751a5738d3a650b2575b2be769",
+    "url": "/tedxcornell/static/media/booklet.e164b075.jpg"
+  },
+  {
+    "revision": "b7dcc65e91a36be16a3815dc26f0642e",
+    "url": "/tedxcornell/static/media/yamatai2.b7dcc65e.jpg"
+  },
+  {
+    "revision": "1fe5775476ad3c19e845e9601376bf52",
+    "url": "/tedxcornell/static/media/molc.1fe57754.jpg"
+  },
+  {
+    "revision": "bf7df121aca609756950de11ee094275",
+    "url": "/tedxcornell/static/media/kirkland.bf7df121.jpg"
+  },
+  {
+    "revision": "d70769ec3476807dc8bbd5b76a6d804e",
+    "url": "/tedxcornell/static/media/Sofiya_Tsenter.d70769ec.png"
+  },
+  {
+    "revision": "b9fecbc11dc388cf5f18b3296075b125",
+    "url": "/tedxcornell/static/media/adam2.b9fecbc1.jpg"
+  },
+  {
+    "revision": "ea44b59bc8658d41f60fb6f8bc04b0df",
+    "url": "/tedxcornell/static/media/eri.ea44b59b.png"
+  },
+  {
+    "revision": "20b6e7cba84c91d28ce65c531ab944ec",
+    "url": "/tedxcornell/static/media/emilychen.20b6e7cb.jpeg"
+  },
+  {
+    "revision": "0ff1531e1b14c54ddb1aaf2bc28fd77c",
+    "url": "/tedxcornell/static/media/Yeonsuk_Kim.0ff1531e.jpg"
+  },
+  {
+    "revision": "1b28c4550debebbfe21c1727e68181b3",
+    "url": "/tedxcornell/static/media/Akshu.1b28c455.png"
+  },
+  {
+    "revision": "2fd94d9f9b108c3154167908952985e4",
+    "url": "/tedxcornell/static/media/Tianyi.2fd94d9f.jpg"
+  },
+  {
+    "revision": "74cc4b2226e7d441581de7f71bc5898a",
+    "url": "/tedxcornell/static/media/liz_gunner.74cc4b22.jpg"
+  },
+  {
+    "revision": "d2b2a6d98e138c12001ccfa1bee40b6f",
+    "url": "/tedxcornell/static/media/hebani.d2b2a6d9.jpeg"
+  },
+  {
+    "revision": "89368f6923cf9fc6b60a78de8b84fb10",
+    "url": "/tedxcornell/static/media/uzair.89368f69.jpg"
+  },
+  {
+    "revision": "3ff49b3302a6f5db457cb7ba37a890a3",
+    "url": "/tedxcornell/static/media/audience1.3ff49b33.jpg"
+  },
+  {
+    "revision": "c9edbc72a0713ad1cd516b371f555cdd",
+    "url": "/tedxcornell/static/media/teamImg.c9edbc72.jpg"
+  },
+  {
+    "revision": "3c37b3e8a054a119184060eaf41860da",
+    "url": "/tedxcornell/static/media/Black_TEDx_Logo_small.3c37b3e8.png"
+  },
+  {
+    "revision": "4b77aa91209d8259fdaf20787411bdd3",
+    "url": "/tedxcornell/static/media/cornellCals.4b77aa91.png"
+  },
+  {
+    "revision": "b96e127aa08b36670ab03b1e129f1bca",
+    "url": "/tedxcornell/static/media/cornellEng_logo.b96e127a.png"
+  },
+  {
+    "revision": "d4e21943d8789d449308cf48fcb23468",
+    "url": "/tedxcornell/static/media/firelight.d4e21943.png"
+  },
+  {
+    "revision": "584034afa28f1ad4d001ff88944a978e",
+    "url": "/tedxcornell/static/media/painted-x.584034af.png"
+  },
+  {
+    "revision": "10451c6ec50e80419ad5130522a65d9b",
+    "url": "/tedxcornell/static/media/main_bg.10451c6e.png"
+  },
+  {
+    "revision": "7f412a10a3733edb5d2f8025cd135584",
+    "url": "/tedxcornell/static/media/The_X.7f412a10.png"
+  },
+  {
+    "revision": "e6dc4d17dde4b25df7d2900ad8b751b8",
+    "url": "/tedxcornell/static/media/pizzaro2.e6dc4d17.jpg"
+  },
+  {
+    "revision": "1f3b5c93ed0e89a51eb342e0d0e71cb2",
+    "url": "/tedxcornell/static/media/jessenitai.1f3b5c93.jpg"
+  },
+  {
+    "revision": "ca1410bf79aa3ef6519f",
+    "url": "/tedxcornell/static/css/1.8ac4a7d7.chunk.css"
+  },
+  {
+    "revision": "25a23fea8a2679a854cdb2c0d21a9270",
+    "url": "/tedxcornell/index.html"
+  }
+];

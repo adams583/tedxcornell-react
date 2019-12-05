587f8ef9a7ed836e595edbe42d321f2f8ac39a9c
Adam Skrocki <ams792@cornell.edu>
2019-12-05 12:11:22 -0500

    Updated routing (exact path match for speakers page)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from "./home/Home";
 import Events from "./events/Events";
 import About from "./about/About";
 import Apply from "./apply/Apply";
-import Speakers from "./speakers/Speakers"; 
+import Speakers from "./speakers/Speakers";
 import AOS from "aos";
 import ErrorPage from "./error/ErrorPage";
 import "aos/dist/aos.css";
@@ -42,7 +42,7 @@ class App extends Component {
               <Route path="/about" component={About} />
               <Route path="/apply" component={Apply} />
               <Route path="/sponsors" component={Sponsors} />
-              <Route path="/speakers" component={Speakers} />
+              <Route exact path="/speakers" component={Speakers} />
               <Route exact path="/faq" component={FAQ} />
               {Object.keys(speakers_2019).map(key => (
                 <Route

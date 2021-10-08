f231c83368028cec6e1e64d1d6a0427387b84b14
Adam Skrocki <adam.skrocki@gmail.com>
2021-10-08 19:44:50 -0400

    added ts

---
+++ src/common/ImageTextOverlap.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./ImageTextOverlap.css";
+import { Image } from "react-bootstrap";
+import CustomButton from "./buttons/CustomButton";
+import ScrollMove from "./ScrollMove";
+
+
+interface ImageTextOverlapProps {
+  content: string;
+  title: string;
+  src: string;
+  button: string;
+  buttonHref: string;
+}
+
+export const ImageTextOverlap = React.memo(function ImageTextOverlapFn({ src, title, content, button, buttonHref }: ImageTextOverlapProps) {
+  return (
+    <div className="image-text-overlap">
+      <div className="ito-img">
+        <Image
+          src={src}
+          alt="Image not rendering"
+          responsive
+        />
+      </div>
+
+      <div className="ito-txt">
+        <ScrollMove>
+          <div className="long-left-border">
+            <h3>{title}</h3>
+            <p>{content}</p>
+            <CustomButton
+              text={button}
+              color="#08B2E3"
+              lightText
+              href={buttonHref}
+            />
+          </div>
+        </ScrollMove>
+      </div>
+    </div>
+  )
+})
+

e2bb235f756422681da458b75020fc4f89d9363e
ap736 <ap736@cornell.edu>
2022-01-07 14:17:28 -0500

    adding to events page

--- src/events/Events.js
+++ src/events/Events.js
@@ -9,6 +9,7 @@ import jonathan_pic from "../img/jonathan.jpg"
 import cat_pic from "../img/cat.jpg";
 import john_pic from "../img/john.jpg";
 import ImageTextOverlay from "../common/ImageTextOverlay";
+import uproot from "../img/website-background.png"
 import page_img from "../img/team2019.jpg"
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -20,63 +21,134 @@ import Stack from "react-bootstrap/Stack"
 import Collapse from 'react-bootstrap/Collapse'
 
 import SpeakerRow from "./SpeakerRow";
-import speaker_info from "../events/speaker_info.json";
+import speaker_info from "./speakerInfo";
 
-const pictures = [daniel_pic, marcela_pic, larry_pic, ishan_pic]
-pictures.forEach((pic, i) => {
-  speaker_info[i]["img"] = pic
+// const pictures = [daniel_pic, marcela_pic, larry_pic, ishan_pic]
+// pictures.forEach((pic, i) => {
+//   speaker_info[i]["img"] = pic
+// }
+// )
+
+function Countdown() {
+  const [countDown, setCountDown] = React.useState(0);
+  const [runTimer, setRunTimer] = React.useState(true);
+  const currentDate = new Date();
+  const eventDate = new Date("4/16/2022")
+
+  function getTimeDiff() {
+    const diffInMs = eventDate - currentDate;
+    console.log(diffInMs / 1000);
+
+    return Math.floor(diffInMs / 1000);
+  }
+
+  React.useEffect(() => {
+    let timerId;
+
+    if (runTimer) {
+      setCountDown(getTimeDiff());
+      timerId = setInterval(() => {
+
+        setCountDown((countDown) => countDown - 1);
+      }, 1000);
+    } else {
+      clearInterval(timerId);
+    }
+
+    return () => clearInterval(timerId);
+  }, [runTimer]);
+
+  React.useEffect(() => {
+    if (countDown < 0 && runTimer) {
+      console.log("expired");
+      setRunTimer(false);
+      setCountDown(0);
+    }
+  }, [countDown, runTimer]);
+
+  // const togglerTimer = () => setRunTimer((t) => !t);
+  function getSeconds() {
+    return countDown % 60;
+  }
+  function getMinutes() {
+    return Math.floor(countDown / 60) % 60
+  }
+
+  function getHours() {
+    return Math.floor(countDown / (60 * 60)) % 24
+  }
+
+  function getDays() {
+    return Math.floor(countDown / (60 * 60 * 24))
+  }
+
+  function convertToString(timeValue) {
+    return String(timeValue).padStart(2, 0);
+  }
+
+  const seconds = convertToString(getSeconds())
+  const minutes = convertToString(getMinutes())
+  const hours = convertToString(getHours())
+  const days = convertToString(getDays())
+  return (
+    <div className="text-center countdown">
+      <Stack>
+        <h1>
+          {days} : {hours} : {minutes} : {seconds}
+        </h1>
+        <h4>Days : Hours : Min : Sec</h4>
+      </Stack>
+    </div>
+  );
 }
-)
 
-function SpeakerCard() {
+
+
+function SpeakerCard(props) {
   const [open, setOpen] = useState(false);
 
-  return (<Col className="" md={10} >
-    <Row className="">
-      <Col className="">
-        <Row className=" text-white justify-content-center" >
-          <Col md className="p-0">
-            <Image fluid style={{ height: '100%' }} src={daniel_pic}></Image>
-          </Col>
-          <Col md={5} className="px-4 " style={{ backgroundColor: 'black' }}>
-            <div className="py-5">
-              <Stack className=" " gap={3}>
-                <h1>Daniel James II</h1>
-                <p>Podcast & Radio Host, Student Leader</p>
-                <Button onClick={() => setOpen(!open)}
-                  aria-controls="example-collapse-text"
-                  aria-expanded={open}
-                  variant="danger" >Learn More</Button>
-              </Stack>
-            </div>
+  return (
+    <Row className={"py-4 " + (props.onRight ? "flex-row-reverse" : '')}>
+      <Col md={11} >
+        <Row className={props.onRight ? "flex-row-reverse" : ''}>
+          <Col>
+            <Row className=" text-white justify-content-center " >
+              <Col md className="p-0">
+                <Image fluid style={{ height: '100%' }} src={props.img}></Image>
+              </Col>
+              <Col md={5} className="px-4 text-white justify-content-center" style={{ backgroundColor: 'black' }}>
+                <div className="py-5">
+                  <Stack gap={3}>
+                    <h1>{props.name}</h1>
+                    <p>{props.info}</p>
+                    <Button onClick={() => setOpen(!open)}
+                      aria-controls="card-collapse-text"
+                      aria-expanded={open}
+                      variant="danger" >Learn More</Button>
+                  </Stack>
+                </div>
+              </Col>
+            </Row>
           </Col>
 
+          <Col md className={'p-0 ' + (props.onRight ? "d-flex align-items-end  flex-column" : 'd-flex align-items-begin flex-column')}>
+            <Collapse style={{ height: '100%' }} in={open} dimension="width">
+              <div id="card-collapse-text " style={{ height: '100%' }}>
+                <Card body className="text-center" style={{ width: '35em', height: '100%' }}>
+                  <Card.Title as="h4">
+                    <u>{props.speech}</u>
+                  </Card.Title>
+                  <Card.Text as="p" className="speech-info">
+                    {props.speechInfo}
+                  </Card.Text>
+                </Card>
+              </div>
+            </Collapse>
+          </Col>
         </Row>
       </Col>
-
-      <Col md className='p-0'>
-        <Collapse in={open} dimension="width">
-          <div id="example-collapse-text " style={{ height: '100%' }}>
-            <Card body className="text-center"style={{ width: '23em', height: '100%' }}>
-              <Card.Title as="h1">
-                Speech Title Here
-              </Card.Title>
-              <Card.Text as="p">
-                Nemo enim ipsam voluptatem quia voluptas
-                sit aspernatur aut odit aut fugit, sed quia consequuntur
-                magni dolores eos qui ratione voluptatem sequi nesciunt.
-                Neque porro quisquam est. Nemo enim ipsam voluptatem quia voluptas
-                sit aspernatur aut odit aut fugit, sed quia consequuntur
-                magni dolores eos qui ratione voluptatem sequi nesciunt.
-                Neque porro quisquam est.
-              </Card.Text>
-            </Card>
-          </div>
-        </Collapse>
-
-      </Col>
     </Row>
-  </Col>);
+  );
 
 }
 
@@ -86,30 +158,47 @@ class Events extends Component {
   render() {
     return (
       <div>
-        <ImageTextOverlay
+        {/* <ImageTextOverlay
           title="Meet Our Speakers"
           body="Each of our events highlights the contributions, experiences,
            and ideas of our amazing speakers. Learn more about the speakers for our upcoming event!"
           img={page_img}
         >
-        </ImageTextOverlay>
-        <Container className="py-5">
-          <SpeakerCard></SpeakerCard>
+        </ImageTextOverlay> */}
+        <div style={{
+          height: '30em', backgroundImage: `url(${page_img})`, backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover'
+        }} className="d-flex align-items-center">
+          <Container className="d-flex align-items-center text-white">
+            <div className="mx-auto">
+              <Countdown ></Countdown>
+            </div>
+          </Container>
+        </div>
+        <Container className="py-3">
+          <div className="text-center py-4">
+            <span className="p-3" id="year"><u>2020 Speakers</u></span>
+          </div>
         </Container>
-        {/* <Container fluid >
-          {
-            speaker_info.map((speaker, i) =>
-
-              <SpeakerRow
-                color={speaker.color}
-                speaker={speaker.name}
-                info={speaker.info}
-                img={speaker.img}
-                imageIsRight={(i % 2) == 1}
-              ></SpeakerRow>
-            )
-          }
-        </Container> */}
+        <Container fluid className="event-info">
+          <Row className="justify-content-center">
+            <Col md={5} className="py-4 pr-4 border">
+              <Stack>
+                <h1>2022: <span id="event-name"><u>UnMuted</u></span></h1>
+                <h4><span style={{ color: 'red' }}><b>TEDx</b></span>Cornell</h4>
+              </Stack>
+            </Col>
+            <Col className="border" md={6}>
+              <Image fluid src={uproot}></Image>
+            </Col>
+          </Row>
+        </Container>
+        <Container className="py-3">
+          {speaker_info.map((speaker, i) =>
+            <SpeakerCard {...speaker} onRight={i % 2 != 0}></SpeakerCard>
+          )}
+        </Container>
         <div className="text-white" style={{ backgroundColor: "red", height: "100%" }}>
           <div className="container">
             <div className="row py-3">
@@ -122,7 +211,7 @@ class Events extends Component {
             </div>
           </div>
         </div>
-      </div>
+      </div >
     );
   }
 }
---
+++ src/events/speakerInfo.js
@@ -0,0 +1,54 @@
+
+let info = [
+  {
+    "name": "Daniel James II",
+    "info": "Podcast & Radio Host, Student Leader",
+    "img": require("../img/daniel_pic.jpg"),
+    "speech": "Why You Should Never Tell a Black Child They Act White",
+    "speechInfo": `Born in Marion, South Carolina, a rural working class town,
+     Daniel James II gained his place as a junior at Cornell University,
+      carrying with him to the Ivy League a desire to better the lives of
+      people. As the creator and host of Black Voices on the Hill, 
+      a podcast & radio show, he aims to amplify the voices of black
+      students, alumni, and city leaders. Inspired by the deaths of
+      George Floyd, Ahmaud Arbery, and Breonna Taylor, he has been 
+      able to reach thousands in upstate New York, talking weekly 
+      about racism, sexism, and activism, with guests ranging from TikTok 
+    sensations, investment fund creators, to the city mayor himself. As a
+     mentor at a local carceral facility and president of the Labor School
+     student body, Daniel has skillfully used his voice over the airwaves
+    to serve the public and reach the masses. This spring, he is an Emerging 
+    Leader intern for the Congressional Black Caucus.`
+  },
+  {
+    "name": "Marcela OT'Alora",
+    "info": "Psychotherapist, Artist",
+    "img": require("../img/marcela.jpg"),
+    "speech": "The Healing Power of MDMA through Reconnection",
+    "speechInfo": `In her private practice she works in partnership 
+    with clients to nourish awareness and connection to innate essence.
+  For over 30 years her interest and focus on trauma has led her
+  to understand the healing process as an intimate re-connection to self, 
+  others, and the world, through compassion, acceptance, and honoring of 
+  each other’s human spirit. In addition to working with trauma and PTSD, 
+  she has dedicated her professional life to teaching, and research. With 
+  clients she uses art as a vehicle for deepening and processing experiences 
+  through non-verbal autobiographical storytelling. For the Multidisciplinary 
+  Association for Psychedelic Studies (MAPS), Marcela has worked as therapist 
+  and Principal Investigator for various studies weaving psychotherapy and MDMA 
+  for the treatment of PTSD. Additionally, she is a trainer and supervisor 
+  for MAPS’ MDMA-assisted psychotherapy training program.`
+  },
+  {
+    "name": "Person Z",
+    "info": "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.",
+    "color": "#f39595"
+  },
+  {
+    "name": "Person A",
+    "info": "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.",
+    "color": "#f1b4b4"
+  }
+]
+
+export default info;
